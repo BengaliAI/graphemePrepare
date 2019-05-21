3d1994179d352d0c1e921724fbe2361cb22ebad5
shouborno <shouborno@ieee.org>
2019-05-21 19:50:26 +0600

    Decluttering

--- codes/extract.m
+++ codes/extract.m
@@ -15,7 +15,7 @@ mask = imbinarize(rgb2gray(imread([refPath '/' 'mask.png'])));
 bw = bwareaopen(mask,800);
 s = regionprops(bw,'BoundingBox');
 width_s = s(1).BoundingBox(3);
-linethickness = .02*width_s; 
+linethickness = floor(.02*width_s); 
 % Adding 24% of height and reducing yTop by 12% of height
 for i=1:length(s)
     s(i).BoundingBox(2)=s(i).BoundingBox(2)-.12*s(i).BoundingBox(4);
@@ -88,13 +88,10 @@ for idx=1:length(files)
         grapheme = imcrop(rec,s{i});
         % We'll find long straight lines (of the range of 90% of width_s)
         % And remove them
-        newgrapheme=imcomplement(imbinarize(rgb2gray(grapheme),1.3*graythresh(rgb2gray(grapheme))));
-        % imshow(newgrapheme);
+        newgrapheme=imcomplement(imbinarize(rgb2gray(grapheme),1.2*graythresh(rgb2gray(grapheme))));
         %% Step 1 : Hough transform to find lines (needs optimization)
-        % Currently doesn't work at all, due to imbalance in line widths 
-        % Above and below
         [H,T,R] = hough(newgrapheme);
-        P  = houghpeaks(H,64,'threshold',ceil(0.4*max(H(:))));
+        P  = houghpeaks(H,64,'threshold',ceil(0.5*max(H(:))));
         lines = houghlines(newgrapheme,T,R,P,'FillGap',.1*width_s,'MinLength',.9*width_s);
         
         %% Step 2: We delete the lines
