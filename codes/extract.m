c54b2cb0dc69f3efe063c06ae62ca5b112e49c3f
shouborno <shouborno@ieee.org>
2019-05-21 19:32:02 +0600

    Fixed line detection and deletion
    Needs further optimization

--- codes/extract.m
+++ codes/extract.m
@@ -14,11 +14,12 @@ formIDs = 1:16;
 mask = imbinarize(rgb2gray(imread([refPath '/' 'mask.png'])));
 bw = bwareaopen(mask,800);
 s = regionprops(bw,'BoundingBox');
-width_s=s(1).BoundingBox(3);
-% Adding 20% of height and reducing yTop by 10% of height
+width_s = s(1).BoundingBox(3);
+linethickness = .02*width_s; 
+% Adding 24% of height and reducing yTop by 12% of height
 for i=1:length(s)
-    s(i).BoundingBox(2)=s(i).BoundingBox(2)-.1*s(i).BoundingBox(4);
-    s(i).BoundingBox(4)=s(i).BoundingBox(4)+.2*s(i).BoundingBox(4);
+    s(i).BoundingBox(2)=s(i).BoundingBox(2)-.12*s(i).BoundingBox(4);
+    s(i).BoundingBox(4)=s(i).BoundingBox(4)+.24*s(i).BoundingBox(4);
 end
 s = struct2cell(s);
 s= s';
@@ -87,21 +88,20 @@ for idx=1:length(files)
         grapheme = imcrop(rec,s{i});
         % We'll find long straight lines (of the range of 90% of width_s)
         % And remove them
-        
-        newgrapheme=imbinarize(rgb2gray(grapheme),graythresh(rgb2gray(grapheme)));
-        imshow(newgrapheme);
+        newgrapheme=imcomplement(imbinarize(rgb2gray(grapheme),1.3*graythresh(rgb2gray(grapheme))));
+        % imshow(newgrapheme);
         %% Step 1 : Hough transform to find lines (needs optimization)
         % Currently doesn't work at all, due to imbalance in line widths 
         % Above and below
-        [H,T,R] = hough(newgrapheme,'Theta',0);
-        P  = houghpeaks(H,2,'threshold',ceil(0.3*max(H(:))));
-        lines = houghlines(newgrapheme,T,R,P,'FillGap',.02*width_s,'MinLength',.95*width_s);
+        [H,T,R] = hough(newgrapheme);
+        P  = houghpeaks(H,64,'threshold',ceil(0.4*max(H(:))));
+        lines = houghlines(newgrapheme,T,R,P,'FillGap',.1*width_s,'MinLength',.9*width_s);
         
         %% Step 2: We delete the lines
         
         for k = 1:length(lines)
            xy = [lines(k).point1; lines(k).point2];
-           grapheme(min(xy(1,1),xy(2,1)):max(xy(1,1),xy(2,1)),min(xy(1,2),xy(2,2)):max(xy(1,2),xy(2,2)),:)=256;
+           grapheme((min(xy(1,2),xy(2,2))-linethickness):(max(xy(1,2),xy(2,2))+linethickness),min(xy(1,1),xy(2,1)):max(xy(1,1),xy(2,1)),:)=256;
         end
         filename = [targetPath '/' char(gt(i)) '/' source '_' char(split(1:end-1)) '.png'];
         imwrite(grapheme,filename);
