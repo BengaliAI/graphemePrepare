1ad75838bf04a3fdabebed02f4f035f76bf8eb9e
AhmedImtiazPrio <ahmed.imtiaz.prio@gmail.com>
2019-05-27 03:46:31 +0600

    purge also purges error folder

--- data/extracted/purge.py
+++ data/extracted/purge.py
@@ -8,3 +8,10 @@ for folder in folders:
 		if file.split('.')[-1] == 'txt':
 			continue
 		os.remove(os.path.join(folder,file))
+
+folder = os.path.join('..','error')
+files = os.listdir(folder)
+for file in files:
+	if file.split('.')[-1] == 'txt':
+		continue
+	os.remove(os.path.join(folder,file))
